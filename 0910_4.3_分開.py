8cf01120278636e5c9351684ce895b586a1e20c1
Stronglian <strong.85.wu@gmail.com>
2018-09-17 13:27:42 +0800

    edit

--- 0910_4.3_分開.py
+++ 0910_4.3_分開.py
@@ -67,24 +67,24 @@ class MontyHallProblem():
     
     def Analysis_noChange(self):
         print('統計')
-        self.getCarNum_noChange = 0
+        getCarNum_noChange = 0
         for i in range(self.frequency):
         #    print(ansArr[i])
             ansTemp = self.ansArr[i][0:3]
             partiTemp = int(self.ansArr[i][self.door_amount])
             if ansTemp[partiTemp] ==1:
-               self.getCarNum_noChange += 1
-        print('If no change:',  round(self.getCarNum_noChange * 100 / float(self.frequency), 2), "%")
+               getCarNum_noChange += 1
+        print('If no change:',  round(getCarNum_noChange * 100 / float(self.frequency), 2), "%")
     def Analysis_change(self):
         print('統計')
-        self.getCarNum_Change = 0
+        getCarNum_Change = 0
         for i in range(self.frequency):
         #    print(ansArr[i])
             ansTemp = self.ansArr[i][0:3]
             partiNewTemp = int(self.ansArr[i][self.door_amount+1])
             if ansTemp[partiNewTemp] ==1:
-               self.getCarNum_Change += 1
-        print('If change:',     round(self.getCarNum_Change   * 100 / float(self.frequency), 2), "%")
+               getCarNum_Change += 1
+        print('If change:',     round(getCarNum_Change   * 100 / float(self.frequency), 2), "%")
     def mainGame_noChange(self):
         if self.reBuild or (self.door_car_npz not in os.listdir('./')):
             self.makeTheTable()
